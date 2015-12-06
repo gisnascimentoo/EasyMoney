d7d6c7aaaad2d363c15aec822a7a96f87cecfb71
sergiozjr <zarthjr@gmail.com>
2015-12-06 14:46:24 -0200

    Enum com o range do perfil e Enum com o tipo do perfil

--- src/model/PerfilCliente.java
+++ src/model/PerfilCliente.java
@@ -1,21 +1,15 @@
 package model;
-//DEFINE OS LIMITES SUPERIORES PARA CADA PERFIL DE CLIENTE 
-public enum PerfilCliente {
 
-	 PERFIL_A_SUP (1500), 
-	 PERFIL_B_INF (1500.01),
-	 PERFIL_B_SUP (5000),
-	 PERFIL_C_INF (5000.01),
-	 PERFIL_C_SUP (10000),
-	 PERFIL_D_INF (10000.01);
+public enum PerfilCliente {
+	PERFIL_A("Perfil A"), PERFIL_B("Perfil B"), PERFIL_C("Perfil C"), PERFIL_D("Perfil D");
 	
-	private final double perfil;
+	private final String nomePerfil;
 	
-    private PerfilCliente (double perfil) {
-        this.perfil = perfil;
+    private PerfilCliente(String nomePerfil) {
+        this.nomePerfil = nomePerfil;
     }
 
-    public double getName() {
-        return perfil;
+    public String getName() {
+        return nomePerfil;
     }
 }
