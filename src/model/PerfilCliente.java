587335b569bb0fe5a29e25181e9024502fdb1e2d
sergiozjr <zarthjr@gmail.com>
2015-12-07 13:39:51 -0200

    Alterações no perfil do Cliente, adicionados valores de troca de faixa

--- src/model/PerfilCliente.java
+++ src/model/PerfilCliente.java
@@ -1,15 +1,26 @@
 package model;
 
 public enum PerfilCliente {
-	PERFIL_A("Perfil A"), PERFIL_B("Perfil B"), PERFIL_C("Perfil C"), PERFIL_D("Perfil D");
+	//Nome, Teto valor, Parcelas Min, Parcelas Max
+	
+	PERFIL_A("Perfil A", 1500), 
+	PERFIL_B("Perfil B", 5000), 
+	PERFIL_C("Perfil C", 10000), 
+	PERFIL_D("Perfil D", Double.MAX_VALUE);
 	
 	private final String nomePerfil;
+	private final double valorMaximo;
 	
-    private PerfilCliente(String nomePerfil) {
+    private PerfilCliente(String nomePerfil, double valorMaximo) {
         this.nomePerfil = nomePerfil;
+        this.valorMaximo = valorMaximo;
     }
 
     public String getName() {
         return nomePerfil;
     }
-}
+    
+    public double getValorMaximo(){
+    	return valorMaximo;
+    }
+  }
